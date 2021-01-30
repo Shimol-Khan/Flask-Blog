c1be147e0f0c40ed472277cc9a57a209067dafab
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-01-30 17:12:48 +0800

    Finalize Fake Login

--- app.py
+++ app.py
@@ -34,9 +34,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'shimolkhan.dev@gmail.com' and form.password.data == '67321000':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessfull!', 'danger')
     return render_template('login.html', title='Login', form=form )
 
 if __name__ == '__main__':
