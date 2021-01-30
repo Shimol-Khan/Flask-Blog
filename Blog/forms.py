7b6469300f8a142c33acfc8cb222286cb5ddcf95
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-01-30 19:17:41 +0800

    User Auth - 1

--- Blog/forms.py
+++ Blog/forms.py
@@ -1,48 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from Blog.models import User
+
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[
-                DataRequired(),
-                Length(min=6, max=32)
-            ])
-
-    contact_number = StringField('Contact Number', validators=[
-                DataRequired(),
-                Length(min=11)
-            ])
-
-    address = StringField('Address', validators=[
-                DataRequired(),
-                Length(min=16)
-            ])
-    
-    email = StringField('Email', validators=[
-                DataRequired(), email()
-            ])
-    
-    password = PasswordField('Password', validators=[
-                DataRequired()
-                ])
-
-    confirm_password = PasswordField('Confirm Password', validators=[
-                DataRequired(),
-                EqualTo('password')
-    ])
-
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[
-                DataRequired(), email()
-            ])
-    
-    password = PasswordField('Password', validators=[
-                DataRequired()
-                ])
-
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-
     submit = SubmitField('Login')
--- Blog/models.py
+++ Blog/models.py
@@ -1,25 +1,30 @@
 from datetime import datetime
-from Blog import db
+from Blog import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    contact_number = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    address = db.Column(db.Text(256), nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.name}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    category = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    body = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
