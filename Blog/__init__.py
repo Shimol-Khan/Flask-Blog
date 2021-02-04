7c04a6958ae7544739ec1143894333d19adebd6e
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-02-04 17:56:17 +0800

    Integrated Python DotEnv

--- Blog/__init__.py
+++ Blog/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '87809ede0894f81ebf0d1ff694670266'
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
